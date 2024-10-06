93c3e481312c65f6d393c163d88832e908526073
Paz Barda <paz.barda@intel.com>
2024-10-06 11:13:46 +0300

    maven project + player.csv

---
+++ src/main/java/com/pazbarda/playerapi/PlayerApiApplication.java
@@ -0,0 +1,13 @@
+package com.pazbarda.playerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pazbarda/playerapi/PlayerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pazbarda.playerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
